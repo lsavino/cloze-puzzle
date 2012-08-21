02f14b0001c091e72c52854a80be802068b2fa30
Laura Savino <lsavino@ubermind.com>
2012-08-20 21:04:29 -0700

    Add and use function to extract every 5th word from list of words

--- main.hs
+++ main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.List
+import Data.Sequence
 import Data.String
 import System.Environment
 import System.IO
@@ -8,4 +10,15 @@ main :: IO ()
 main = do
     (filename:args) <- getArgs
     fullText  <- (readFile filename)
-    putStrLn fullText
+    let sequencedWords = fromList $ words fullText
+        (blankedText, removedWords) = foldlWithIndex blankWords ([], []) sequencedWords
+        displayText = concat $ intersperse " " blankedText
+    putStrLn displayText
+    putStrLn $ "Removed words: " ++ (concat $ intersperse " " removedWords)
+
+
+blankWords :: ([String], [String]) -> Int -> String -> ([String], [String])
+blankWords (remainingWords, removedWords) i word =
+    if (rem i 5) == 0
+    then (remainingWords ++ ["_____"], removedWords ++ [word])
+    else (remainingWords ++ [word], removedWords)
